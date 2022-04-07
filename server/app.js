955c6718c03e25c0e0aa97337429d8ba5b79cb7b
Owoeye1321 <owoeye1321@gmail.com>
2022-04-07 15:37:03 -0700

    added few block of code

--- server/app.js
+++ server/app.js
@@ -1,14 +1,35 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
+const auth = require('./routes/auth')
  const PORT = process.env.PORT || 5000
- app.get('/',( req, res ) =>{
-   res.send('hello world')
-   console.log('Nodemon working well')
+
+
+ app.use('/',auth)
+ 
+ 
+ app.get('/hello',(req, res ) =>{
+   res.send('Home page  connected to server successfully')
+ })
+ 
+ app.get('/people', (req, res ) => {
+  res.json
+  ([{
+      id:1,
+      name:"gtwatt",
+      password:"password" 
+  },
+  {
+      id:2,
+      name:"owoeye",
+      password:"password"
+  }])
  })
 
-
-
-  
+ app.all("*",(req, res, next)=>{
+  res.send("Hello there, you seem to be on another undefined route")
+  next()
+})
 app.listen(PORT, () => {
     console.log('Listening to port' + ' ' + PORT)
   })  
