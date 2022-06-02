2404c8fcb6b0e2c80f48e05854870de257923ded
Owoeye1321 <owoeye1321@gmail.com>
2022-06-02 10:23:58 -0700

    new commit

--- server/routes/singlePayment.js
+++ server/routes/singlePayment.js
@@ -1,37 +1,41 @@
-const express = require('express')
-const router = express.Router()
-const mysqlConnection = require('mysql')
-const con = mysqlConnection.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'addriggo',
-  })
-  router.post('/', (req, res )=>{
+if (process.env.NODE_ENV !== "production") require('dotenv').config();
+const uri = process.env.ATLAS_URI
+
+const { MongoClient, ServerApiVersion } = require('mongodb')
+      const router = require('express').Router()
+   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+  router.post('/',(req, res )=>{
     const sess = req.session
     sess.productId = req.body.productId
     sess.amount = req.body.amount
     if(sess.user){
      const username = sess.user
-     const amount = req.body.amount
+     const amount = req.body.amou
      const productId = req.body.productId
      const status = 'pending'
-     const sql = "SELECT * FROM pending WHERE customer = ? AND productId = ?"
-     con.query(sql,[username,productId],(outerErr, outerResult) => {
-       if(!outerErr){
-         if(outerResult.length){
-           res.send('exist')
-           console.log(sess.productId)
-           console.log(sess.amount)
-         }else{
-          const innersql = "INSERT INTO pending (customer, amount, productId,status) VALUES(?, ?, ?, ?)"
-          con.query(innersql,[username, amount, productId,status],(err, result)=>{
-            if(!err){ res.send('success'); console.log(sess.productId);  console.log(sess.amount)}else{ console.log(err);} 
-          })
-         }
-       }else{console.log(outerErr)}
-     })
-    
+     client.connect(async err => {
+     const collection = client.db("ecommerce").collection("pending");
+     const singlePayment = await collection.find({customer:username, productId:productId}).toArray()
+     if(singlePayment.length){
+        res.send('exist')
+        console.log(sess.productId)
+        console.log(sess.amount)
+     }else{
+        const newSinglePayment = await collection.insertOne({customer:username, amount:amount, productId:productId, status:status})
+        if(newSinglePayment){
+          res.send('success')
+          console.log(productId)
+          console.log(sess.amount)
+        }else{
+          console.log('Unable to save details')
+        }
+     }
+
+     client.close();
+    });
+
+  
    }
   })
 
