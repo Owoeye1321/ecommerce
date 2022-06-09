60457ba2f746f7928ffbbbb340cf3ae8ae9f20dd
Owoeye1321 <owoeye1321@gmail.com>
2022-06-09 13:28:48 -0700

    new commit

--- server/routes/removeProduceQty.js
+++ server/routes/removeProduceQty.js
@@ -6,59 +6,34 @@ const { MongoClient, ServerApiVersion } = require('mongodb')
    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
   router.post('/', (req, res )=>{
+      
       if(req.body.productId){
-
           const id = req.body.productId
-             client.connect(async err =>{
-                 const collectionForAddToCart = client.db('ecommerce').collection('addToCart')
-                    const selectQtyAndProductPrice = await collectionForAddToCart.findOne({productId:id})
-                    if(selectQtyAndProductPrice){
-                        selectQtyAndProductPrice.map((key)=>{
-                            const get = parseInt(key.qty)
-                            
-                        })
-                    }
-                client.close();
-             })
-                    const sql = "SELECT qty, productPrice FROM addToCart WHERE productId = ?"
-                    con.query(sql,[id],(err, result)=>{
-                        if(!err){
-                            result.map((value)=>{
-                                const get = parseInt(value.qty)
-                                if (get > 0) {
-                                    const getQty = value.qty - 1
-                                    const getTotal = value.productPrice
-                                    const sum = getQty * getTotal
-                                    const customerId = req.body.productId
-                                    const innerSql = "UPDATE addToCart SET qty = ?, total = ? where productId = ?"
-                                    con.query(innerSql,[getQty,sum,customerId], (err, result)=>{
-                                        if(result){
-                                            res.send('success')
-                                            console.log(result)
-                                        }
-                                    })
-                                }else if(get == 0){
-                                    
-                                    const innestsql = "DELETE FROM addToCart WHERE productId = ?"
-                                    con.query(innestsql,[id],(err, data)=>{
-                                        if(!err){
-                                            res.json({success:'success'})
-                                            console.log(data)
-                                        }
-                                    })
-                                }
-                           
-                               
-                            })
-
-
-                        }else{
-                            console.log('An error has occured'+ err)
+            client.connect(async err =>{
+                const collection = client.db('ecommerce').collection('addToCart')
+                const selectSomeVariableFromAddToCart = await collection.findOne({productId:id})
+                if(selectSomeVariableFromAddToCart){
+                    console.log(selectSomeVariableFromAddToCart)
+                        const getQty = selectSomeVariableFromAddToCart.qty - 1;
+                           const getTotal = selectSomeVariableFromAddToCart.productPrice
+                                const sum = getQty * getTotal
+                             const customerId = req.body.productId
+                         const updateQtyAndTotal = await collection.updateOne({productId:customerId},{$set:{qty:getQty, totalPrice:sum}})
+                        if(updateQtyAndTotal){
+                            res.send('success')
+                            console.log(updateQtyAndTotal)
                         }
-                    })
+                  
+                }else{
+                    console.log('An error has ocurred')
+                }
+                
+            })
           
+          
+      }else{
+          res.send('invalid')
       }
-     
   })
 
   module.exports = router
