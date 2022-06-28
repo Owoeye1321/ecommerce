1e6c8a0d9c1f6c471408c40e3b784eb0561d4404
Owoeye1321 <owoeye1321@gmail.com>
2022-06-28 12:57:47 -0700

    new commit

--- server/routes/queryCart.js
+++ server/routes/queryCart.js
@@ -1,16 +1,10 @@
-if (process.env.NODE_ENV !== "production") require('dotenv').config();
-const uri = process.env.ATLAS_URI
-
-const { MongoClient, ServerApiVersion } = require('mongodb')
+const client = require('../controller/client')   
       const router = require('express').Router()
-   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-  router.get('/',(req, res)=>{
+  router.get('/',async (req, res)=>{
     const sess = req.session
     if(sess.user){
         const customer_name = sess.user
-        client.connect(async err =>{
-            console.log('connnected to mongodb successfully')
             const collection = client.db('ecommerce').collection('addToCart')
             const queryProduct = await collection.find({ customer_name: customer_name, status:'ordered'}).toArray()
             if(queryProduct){
@@ -20,8 +14,6 @@ const { MongoClient, ServerApiVersion } = require('mongodb')
                 console.log('An error has occurred')
                 res.send('Unable to delete Item')
             }
-           
-        })
     }
   })
   module.exports = router
