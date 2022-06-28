9009dee0a457c02a91132dfadda79a7fd4de4024
Owoeye1321 <owoeye1321@gmail.com>
2022-06-28 12:57:23 -0700

    new commit

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,17 +1,10 @@
-if (process.env.NODE_ENV !== "production") require('dotenv').config();
-   const uri = process.env.ATLAS_URI
-
 const router = require('express').Router()
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = require('../controller/client')   
 
-router.post('/',(req, res) =>{
+router.post('/',async (req, res) =>{
    const sess = req.session
          const username = req.body.details.username
     const password = req.body.details.password
-
-      client.connect( async err => {
-         console.log('mongodb database connected successfully')
              const collection = client.db("ecommerce").collection("users");
              const result = await collection.findOne({ username: username, password: password})
       if(result){
@@ -22,10 +15,6 @@ router.post('/',(req, res) =>{
          console.log('user does not exist')
            res.send('invalid')
       }
-     
-      
-      })
-      client.close();
 
 })
 
