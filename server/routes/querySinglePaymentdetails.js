5f7f9baa2e0bc2284dbb754863f3c419ec4d66a9
Owoeye1321 <owoeye1321@gmail.com>
2022-06-28 12:58:11 -0700

    new commit

--- server/routes/querySinglePaymentdetails.js
+++ server/routes/querySinglePaymentdetails.js
@@ -1,12 +1,7 @@
-if (process.env.NODE_ENV !== "production") require('dotenv').config();
-const uri = process.env.ATLAS_URI
-const publicKey = process.env.PUBLIC_PAYSTACK_KEY
-
-const { MongoClient, ServerApiVersion } = require('mongodb')
+const client = require('../controller/client')   
       const router = require('express').Router()
-   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-  router.get('/',(req, res)=>{
+  router.get('/',async (req, res)=>{
    const sess = req.session
    if(sess.user){
     res.json({
@@ -24,32 +19,32 @@ const { MongoClient, ServerApiVersion } = require('mongodb')
        const productId = req.body.productId
         const amount = req.body.amount
       const status = "paid"
-  client.connect(async err=>{
-    const collectionForPending = client.db('ecommerce').collection('pending')
-      const deleteFromPending = await collectionForPending.deleteOne({customer:customer, productId:productId})
-    if(deleteFromPending){
-      const collectionForAddToCart = client.db('ecommerce').collection('addToCart')
-        const updateAddToCart = await collectionForAddToCart.updateOne({customer_name:customer, productId:productId},{status:status})
-          if(updateAddToCart){
-             const collectionForTransaction = client.db('ecommerce').collection('transaction')
-                const InsertNewTransaction  = await collectionForTransaction.insertOne({customer:customer, amount:amount, status:status})
-                  if(InsertNewTransaction){
-                    res.send('Done')
-                  }else{
-                    res.send('Unable to insert transaction')
-                    console.log('Unable to insert transaction')
-                  }
+          const query = async ()=>{
+            const collectionForPending = client.db('ecommerce').collection('pending')
+            const deleteFromPending = await collectionForPending.deleteOne({customer:customer, productId:productId})
+          if(deleteFromPending){
+            const collectionForAddToCart = client.db('ecommerce').collection('addToCart')
+              const updateAddToCart = await collectionForAddToCart.updateOne({customer_name:customer, productId:productId},{status:status})
+                if(updateAddToCart){
+                   const collectionForTransaction = client.db('ecommerce').collection('transaction')
+                      const InsertNewTransaction  = await collectionForTransaction.insertOne({customer:customer, amount:amount, status:status})
+                        if(InsertNewTransaction){
+                          res.send('Done')
+                        }else{
+                          res.send('Unable to insert transaction')
+                          console.log('Unable to insert transaction')
+                        }
+                }else{
+                  res.send('Unable to update to addToCart')
+                  console.log('Unable to Update to addToCart')
+                }
           }else{
-            res.send('Unable to update to addToCart')
-            console.log('Unable to Update to addToCart')
+            res.send('Unable to delete from pending')
+            console.log('Unable to delete from pending')
           }
-    }else{
-      res.send('Unable to delete from pending')
-      console.log('Unable to delete from pending')
-    }
-    
-  })
-  client.close();
+          }
+          query()
+
   }
   })
   module.exports = router
