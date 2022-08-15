68684731685864d1f7541e95fa0b63b6455933db
Owoeye1321 <owoeye1321@gmail.com>
2022-08-15 13:39:25 -0700

    new commit

--- server/routes/login.js
+++ server/routes/login.js
@@ -2,23 +2,39 @@ const router = require('express').Router()
 const client = require('../controller/client')   
 
 router.post('/',async (req, res) =>{
-   const sess = req.session
          const username = req.body.details.username
     const password = req.body.details.password
              const collection = client.db("ecommerce").collection("users");
              const result = await collection.findOne({ username: username, password: password})
       if(result){
-         sess.user = username
-             res.send('success')
-         console.log("user exist")
+         req.session.user = username
+         req.session.save( err =>{
+            if(err){
+               console.log("An error has occured")
+            }else{
+               res.send('success')
+               console.log(req.session.user)
+               console.log(req.session)
+               console.log("The session has been saved and successfully stored")
+            }
+         })
 
-            const interval = setInterval (()=>{
-                       console.log(sess.user)
-                },2000)
-        
-                return()=>{
-                        clearInterval(interval)
-                }
+         // sess.user = username
+         // if(sess.user){
+         //    res.send('success')
+         // console.log("user exist")
+
+         //    const interval = setInterval (()=>{
+         //               console.log(sess.user)
+         //        },60000)
+         //        return()=>{
+         //                clearInterval(interval)
+         //        }
+         // }else{
+         //    res.send('Unable to save')
+         //    console.log("Unable to create a session for the current user")
+         // }
+             
       } else {
          console.log('user does not exist')
            res.send('invalid')
@@ -27,6 +43,3 @@ router.post('/',async (req, res) =>{
 })
 
 module.exports = router
-
-
-
